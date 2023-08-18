a5bc99e3e067d00f057ec19437f85aa7d48cee57
Akshay Verma <47255769+akshayverma1697@users.noreply.github.com>
2023-08-18 18:56:04 -0500

    added styling to add user div

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../UI/Card";
+//import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
     //function when submit button is pressed
@@ -8,13 +10,16 @@ const AddUser = (props) => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"/>
-            <button type="submit">Submit</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" />
+                <button type="submit">Submit</button>
+            </form>
+        </Card>
+
     );
 };
 
