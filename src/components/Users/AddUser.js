037d60f65a061663afde4c81c3394307a1c35eed
Akshay Verma <47255769+akshayverma1697@users.noreply.github.com>
2023-08-18 19:27:22 -0500

    made functionality with input fields and button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+
+    const[enteredUser, setEnteredUser] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     //function when submit button is pressed
     const addUserHandler = (event) => {
         event.preventDefault();
-        console.log('submit button pressed here is the data: ', event.target.value);
-    }
+        console.log(enteredAge, enteredUser)
+    };
+
+    const nameEditHandler = (event) => {
+        console.log('things are being changed: ', event.target.value);
+        setEnteredUser(event.target.value);
+    };
+
+    const ageEditHandler = (event) => {
+        console.log('things are being changed: ', event.target.value);
+        setEnteredAge(event.target.value)
+    };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username" className={classes.label}>Username</label>
-                <input id="username" type="text" className={classes.input}/>
+                <input id="username" type="text" className={classes.input} onChange={nameEditHandler}/>
                 <label htmlFor="age" className={classes.label}>Age (Years)</label>
-                <input id="age" type="number" className={classes.input}/>
+                <input id="age" type="number" className={classes.input} onChange={ageEditHandler}/>
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
